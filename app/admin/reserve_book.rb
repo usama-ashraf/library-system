7cc1afbc56fe08454986e70c1afb44dbf13e878a
Ferhan <ferhannasim@gmail.com>
2018-05-07 03:24:26 +0500

    fixes

--- app/admin/reserve_book.rb
+++ app/admin/reserve_book.rb
@@ -4,8 +4,14 @@ ActiveAdmin.register ReserveBook do
     f.semantic_errors *f.object.errors.keys
     f.inputs 'Information' do
       f.input :status
-      f.input :issue_date
-      f.input :due_date
+      f.input :issue_date ,as: :datepicker,
+          datepicker_options: {
+          min_date: Date.today
+      }
+      f.input :due_date ,as: :datepicker,
+              datepicker_options: {
+                  min_date: Date.today
+              }
       f.input :user_id, :as => :selectize, :collection => User.all.map{|u| ["#{u.email}", u.id]}
       f.input :book_id, :as => :selectize, :collection => Book.all.map{|u| ["#{u.title}", u.id]}
     end
